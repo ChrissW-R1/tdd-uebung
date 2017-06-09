bea7ed8f2d3a306f40686447be5d39a1339593c6
ChrissW-R1 <contact@ChrissW-R1.me>
2017-06-09 10:29:50 +0200

    added OrderManager.submitOrder(Order)

--- src/main/java/de/hsbochum/fbg/kswe/tdd/orders/OrderManager.java
+++ src/main/java/de/hsbochum/fbg/kswe/tdd/orders/OrderManager.java
@@ -1,20 +1,27 @@
 
 package de.hsbochum.fbg.kswe.tdd.orders;
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
- *
  * @author <a href="mailto:m.rieke@52north.org">Matthes Rieke</a>
  */
 public class OrderManager {
-
-    private final ProductDeliveryService delivery;
-    private final List<Product> queuedOrders = new LinkedList<>();
-
-    public OrderManager(ProductDeliveryService delivery) {
-        this.delivery = delivery;
-    }
-    
+	private final ProductDeliveryService delivery;
+	private final List<Order> queuedOrders = new LinkedList<>();
+	
+	public OrderManager(ProductDeliveryService delivery) {
+		this.delivery = delivery;
+	}
+	
+	public Set<Order> getOrders() {
+		return new LinkedHashSet<>(this.queuedOrders);
+	}
+	
+	public void submitOrder(Order order) {
+		this.queuedOrders.add(order);
+	}
 }
--- src/test/java/de/hsbochum/fbg/kswe/tdd/orders/OrderManagerTest.java
+++ src/test/java/de/hsbochum/fbg/kswe/tdd/orders/OrderManagerTest.java
@@ -1,10 +1,32 @@
-
 package de.hsbochum.fbg.kswe.tdd.orders;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 /**
- *
  * @author <a href="mailto:m.rieke@52north.org">Matthes Rieke</a>
  */
 public class OrderManagerTest {
-
+	private static ProductDeliveryService service;
+	private static OrderManager           manager;
+	
+	@Before
+	public void setUp() {
+		service = new ProductDeliveryService();
+		manager = new OrderManager(service);
+	}
+	
+	@Test
+	public void submitOrder() {
+		Customer customer = Mockito.mock(Customer.class);
+		Product  product  = Mockito.mock(Product.class);
+		Order    order    = new Order(customer, 1337, product);
+		
+		manager.submitOrder(order);
+		
+		Assert.assertThat(manager.getOrders().iterator().next(), CoreMatchers.is(order));
+	}
 }
